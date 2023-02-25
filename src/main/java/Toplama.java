bee3af637b3e92718eb95cb96e2b1af713067869
Emre QA <emreonal12@gmail.com>
2023-02-25 23:05:35 +0300

    v1

---
+++ src/main/java/Toplama.java
@@ -0,0 +1,13 @@
+public class Toplama {
+
+    public static void main(String[] args) {
+
+        topla(3,2);
+
+    }
+
+    public static void topla(int a,int b){
+        System.out.println(a+b);
+    }
+
+}
