a7836b891f5a2e3b5a82c5d20135e92fc29398d9
Emre QA <emreonal12@gmail.com>
2023-02-25 23:10:02 +0300

    v2

--- src/main/java/Toplama.java
+++ src/main/java/Toplama.java
@@ -2,12 +2,17 @@ public class Toplama {
 
     public static void main(String[] args) {
 
-        topla(3,2);
+        topla(3,2,5,-10);
 
     }
 
-    public static void topla(int a,int b){
-        System.out.println(a+b);
+    public static void topla(int... a){
+        int toplam = 0;
+        for (int w : a) {
+            toplam+=w;
+        }
+
+        System.out.println("toplam = " + toplam);
     }
 
 }
